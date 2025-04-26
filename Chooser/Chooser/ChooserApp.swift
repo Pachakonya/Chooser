ebdb8750deea7eb8bcf40a8f22e7f53adfae2ed4
Dastan Sugirbay <sugirbay@Dastans-MacBook-Air-2.local>
2025-04-26 17:50:54 +0500

    feat: add project

---
+++ Chooser/Chooser/ChooserApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooserApp.swift
+//  Chooser
+//
+//  Created by Dastan Sugirbay on 26.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChooserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
